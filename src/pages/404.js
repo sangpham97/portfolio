541d54a0a422ba1057bac74b5402e3d3ea69431d
sangpham97 <wunha1997@gmail.com>
2021-09-22 10:48:21 +0700

    update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,10 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import { navLinks1 } from "../util"
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout Links={navLinks1}>
     <h1>404: Not Found</h1>
   </Layout>
 )
