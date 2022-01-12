5cb7d226b67d59ffe554ca6458a4a8bbb2968291
sangpham97 <wunha1997@gmail.com>
2022-01-12 16:02:19 +0700

    changed-avatar

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,7 @@ export default function About() {
         </div>
         <div className="col-span-3 flex justify-center mt-4 md:mt-0">
           <StaticImage
-            src="https://scontent.fsgn6-1.fna.fbcdn.net/v/t1.6435-9/118540546_713167122746593_1838832190750200407_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_ohc=dgUoHoeF79wAX9IyU6E&_nc_ht=scontent.fsgn6-1.fna&oh=0a7de6acc02403abc3bd1c44545f692c&oe=61AB7218"
+            src="../images/sangavatar.jpg"
             className="rounded-full w-72"
           />
         </div>
